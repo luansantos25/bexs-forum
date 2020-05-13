ede42c3bf55c06bb1520e1fb54ed8d57e6f2712c
Luan <luan.dev.santos@gmail.com>
2020-05-12 22:18:48 -0300

    WIP backend new routes

--- back-end/src/routes/question.routes.ts
+++ back-end/src/routes/question.routes.ts
@@ -5,9 +5,17 @@ import Question from '../schemas/Question'
 const questionRouter = Router()
 
 questionRouter.get('/', async (request, response) => {
-  const users = await Question.find()
+  const questions = await Question.find()
 
-  return response.json(users)
+  return response.json(questions)
+})
+
+questionRouter.get('/:id', async (request, response) => {
+  const { id } = request.params
+
+  const question = await Question.findById(id)
+
+  return response.json(question)
 })
 
 questionRouter.post('/', async (request, response) => {
@@ -25,8 +33,6 @@ questionRouter.post('/:id/answers', async (request, response) => {
   const { id } = request.params
   const { text } = request.body
 
-  // const question = await Question.findOne({ _id: id })
-
   const question = await Question.findOneAndUpdate(
     { _id: id },
     { $push: { answers: { text } } },
@@ -34,13 +40,37 @@ questionRouter.post('/:id/answers', async (request, response) => {
   )
 
   return response.json({ test: question })
-
-  // try {
-  //   const user = await Question.create({ user: username, text, answers })
-  //   return response.json(user)
-  // } catch (err) {
-  //   return response.json({ error: err.message })
-  // }
 })
+
+questionRouter.post(
+  '/:questionId/answers/:answerId/likes',
+  async (request, response) => {
+    const { questionId, answerId } = request.params
+    const { username } = request.body
+
+    const question = await Question.findOneAndUpdate(
+      { _id: questionId, answers: { $elemMatch: { _id: answerId } } },
+      { $addToSet: { 'answers.$.likes': username } },
+      { new: true },
+    )
+
+    return response.json({ question })
+  },
+)
+
+questionRouter.delete(
+  '/:questionId/answers/:answerId/likes/:username',
+  async (request, response) => {
+    const { questionId, answerId, username } = request.params
+
+    const question = await Question.findOneAndUpdate(
+      { _id: questionId, answers: { $elemMatch: { _id: answerId } } },
+      { $pull: { 'answers.$.likes': username } },
+      { new: true },
+    )
+
+    return response.json({ question })
+  },
+)
 
 export default questionRouter
